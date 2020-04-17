2e46d7c4b1623adf911f3542ad7b7122de31a6ef
Brontata <guilherme.almeida129@etec.sp.gov.br>
2020-04-17 18:05:07 -0300

    TCC
    TCC desenvolvido em grupo na ETEC Fernando Prestes

---
+++ prjOficinaMecanica/FrmConfigurar.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace prjOficinaMecanica
+{
+    partial class FrmConfigurar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTema = new System.Windows.Forms.ComboBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbtSim = new System.Windows.Forms.RadioButton();
+            this.rbtNao = new System.Windows.Forms.RadioButton();
+            this.cmbForm = new System.Windows.Forms.ComboBox();
+            this.grpSenha = new System.Windows.Forms.GroupBox();
+            this.chbUsarSenha = new System.Windows.Forms.CheckBox();
+            this.txtRepetirSenha = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSenha = new System.Windows.Forms.Button();
+            this.btnbackup = new System.Windows.Forms.Button();
+            this.grpSenha.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tema:";
+            // 
+            // cmbTema
+            // 
+            this.cmbTema.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbTema.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTema.FormattingEnabled = true;
+            this.cmbTema.Items.AddRange(new object[] {
+            "Claro",
+            "Escuro"});
+            this.cmbTema.Location = new System.Drawing.Point(53, 12);
+            this.cmbTema.Name = "cmbTema";
+            this.cmbTema.Size = new System.Drawing.Size(340, 21);
+            this.cmbTema.TabIndex = 1;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(314, 303);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(233, 303);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "&Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Abrir formulário ao inicializar?";
+            // 
+            // rbtSim
+            // 
+            this.rbtSim.AutoSize = true;
+            this.rbtSim.Location = new System.Drawing.Point(191, 44);
+            this.rbtSim.Name = "rbtSim";
+            this.rbtSim.Size = new System.Drawing.Size(42, 17);
+            this.rbtSim.TabIndex = 5;
+            this.rbtSim.Text = "Sim";
+            this.rbtSim.UseVisualStyleBackColor = true;
+            this.rbtSim.CheckedChanged += new System.EventHandler(this.rbtSim_CheckedChanged);
+            // 
+            // rbtNao
+            // 
+            this.rbtNao.AutoSize = true;
+            this.rbtNao.Checked = true;
+            this.rbtNao.Location = new System.Drawing.Point(239, 44);
+            this.rbtNao.Name = "rbtNao";
+            this.rbtNao.Size = new System.Drawing.Size(45, 17);
+            this.rbtNao.TabIndex = 6;
+            this.rbtNao.TabStop = true;
+            this.rbtNao.Text = "Não";
+            this.rbtNao.UseVisualStyleBackColor = true;
+            this.rbtNao.CheckedChanged += new System.EventHandler(this.rbtNao_CheckedChanged);
+            // 
+            // cmbForm
+            // 
+            this.cmbForm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbForm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbForm.Enabled = false;
+            this.cmbForm.FormattingEnabled = true;
+            this.cmbForm.Items.AddRange(new object[] {
+            "Nenhum",
+            "Cliente",
+            "Mecânico",
+            "Orçamento",
+            "Serviço"});
+            this.cmbForm.Location = new System.Drawing.Point(19, 72);
+            this.cmbForm.Name = "cmbForm";
+            this.cmbForm.Size = new System.Drawing.Size(374, 21);
+            this.cmbForm.TabIndex = 7;
+            // 
+            // grpSenha
+            // 
+            this.grpSenha.Controls.Add(this.chbUsarSenha);
+            this.grpSenha.Controls.Add(this.txtRepetirSenha);
+            this.grpSenha.Controls.Add(this.txtSenha);
+            this.grpSenha.Controls.Add(this.label3);
+            this.grpSenha.Enabled = false;
+            this.grpSenha.Location = new System.Drawing.Point(19, 180);
+            this.grpSenha.Name = "grpSenha";
+            this.grpSenha.Size = new System.Drawing.Size(370, 117);
+            this.grpSenha.TabIndex = 8;
+            this.grpSenha.TabStop = false;
+            this.grpSenha.Text = "Definir Senha:";
+            // 
+            // chbUsarSenha
+            // 
+            this.chbUsarSenha.AutoSize = true;
+            this.chbUsarSenha.Checked = true;
+            this.chbUsarSenha.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbUsarSenha.Location = new System.Drawing.Point(10, 86);
+            this.chbUsarSenha.Name = "chbUsarSenha";
+            this.chbUsarSenha.Size = new System.Drawing.Size(134, 17);
+            this.chbUsarSenha.TabIndex = 10;
+            this.chbUsarSenha.Text = "Sempre verificar senha";
+            this.chbUsarSenha.UseVisualStyleBackColor = true;
+            // 
+            // txtRepetirSenha
+            // 
+            this.txtRepetirSenha.Location = new System.Drawing.Point(10, 60);
+            this.txtRepetirSenha.Name = "txtRepetirSenha";
+            this.txtRepetirSenha.PasswordChar = '*';
+            this.txtRepetirSenha.Size = new System.Drawing.Size(354, 20);
+            this.txtRepetirSenha.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(10, 18);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(354, 20);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Repetir Senha:";
+            // 
+            // btnSenha
+            // 
+            this.btnSenha.Location = new System.Drawing.Point(19, 151);
+            this.btnSenha.Name = "btnSenha";
+            this.btnSenha.Size = new System.Drawing.Size(374, 23);
+            this.btnSenha.TabIndex = 9;
+            this.btnSenha.Text = "Alterar Senha";
+            this.btnSenha.UseVisualStyleBackColor = true;
+            this.btnSenha.Click += new System.EventHandler(this.btnSenha_Click);
+            // 
+            // btnbackup
+            // 
+            this.btnbackup.Location = new System.Drawing.Point(19, 112);
+            this.btnbackup.Name = "btnbackup";
+            this.btnbackup.Size = new System.Drawing.Size(374, 23);
+            this.btnbackup.TabIndex = 10;
+            this.btnbackup.Text = "Fazer Backup manual";
+            this.btnbackup.UseVisualStyleBackColor = true;
+            this.btnbackup.Click += new System.EventHandler(this.btnbackup_Click);
+            // 
+            // FrmConfigurar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 338);
+            this.Controls.Add(this.btnbackup);
+            this.Controls.Add(this.btnSenha);
+            this.Controls.Add(this.grpSenha);
+            this.Controls.Add(this.cmbForm);
+            this.Controls.Add(this.rbtNao);
+            this.Controls.Add(this.rbtSim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.cmbTema);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(314, 294);
+            this.Name = "FrmConfigurar";
+            this.Text = "Configurações";
+            this.Load += new System.EventHandler(this.FrmConfigurar_Load);
+            this.grpSenha.ResumeLayout(false);
+            this.grpSenha.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTema;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbtSim;
+        private System.Windows.Forms.RadioButton rbtNao;
+        private System.Windows.Forms.ComboBox cmbForm;
+        private System.Windows.Forms.GroupBox grpSenha;
+        private System.Windows.Forms.TextBox txtRepetirSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSenha;
+        private System.Windows.Forms.CheckBox chbUsarSenha;
+        private System.Windows.Forms.Button btnbackup;
+    }
+}
---
+++ prjOficinaMecanica/FrmLoad.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace prjOficinaMecanica
+{
+    partial class FrmLoad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.loadbar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadbar
+            // 
+            this.loadbar.Location = new System.Drawing.Point(11, 15);
+            this.loadbar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.loadbar.Name = "loadbar";
+            this.loadbar.Size = new System.Drawing.Size(483, 33);
+            this.loadbar.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Location = new System.Drawing.Point(153, 0);
+            this.mainLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(176, 23);
+            this.mainLabel.TabIndex = 1;
+            this.mainLabel.Text = "Saindo com segurança";
+            this.mainLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.mainLabel, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 66);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(483, 31);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // FrmLoad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 108);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.loadbar);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "FrmLoad";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLoad";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar loadbar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
